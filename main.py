826d8689612fe7d301859f4dc7e06b0c819fcf1e
Matthew Guizar <matthewrguizar@gmail.com>
2023-02-26 11:49:49 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from player import Player
+from enemy import Enemy, Troll
+
+matthew = Player("matthew")
+
+
+random_monster = Enemy("Basic Enemy", 12, 1)
+print(random_monster)
+
+random_monster.take_damage(4)
+print(random_monster)
+
+troll = Troll("ug", 18, 1)
+print("ugly troll - {}".format(troll))
